1f7b6a8262e39114bbdcce901ab5562af7fa82a6
freevatar <freevatar@gmail.com>
2015-09-24 01:04:37 +0300

    Delete trash from headers and XML-RPC

--- wp-content/themes/bestway/functions.php
+++ wp-content/themes/bestway/functions.php
@@ -26,4 +26,14 @@ function momt_shortcodes_to_exempt_from_wptexturize($shortcodes){
     $shortcodes[] = 'graphs';
     return $shortcodes;
 }
+
+	remove_action('wp_head', 'rsd_link');
+        remove_action('wp_head', 'wp_generator');
+	remove_action('wp_head', 'rel_canonical');
+	remove_action('wp_head', 'wlwmanifest_link');
+	remove_action('wp_head', 'feed_links', 2);
+	remove_action('wp_head', 'feed_links_extra', 3);
+	remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0 );
+	remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+
 require_once MOM_FW . '/Mobile_Detect.php';
