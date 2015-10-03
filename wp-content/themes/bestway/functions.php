05a75a44da6de05616a7a4b50edef3eadf802d9d
freevatar <freevatar@gmail.com>
2015-10-03 12:33:40 +0300

    Delete deafult quicktags. Fix CSS. Add rule to allow add IMG for contributors

--- wp-content/themes/bestway/functions.php
+++ wp-content/themes/bestway/functions.php
@@ -5,14 +5,28 @@ if (file_exists(get_template_directory() . '/demo/demo.php')) {
 }
  add_filter( 'no_texturize_shortcodes', 'momt_shortcodes_to_exempt_from_wptexturize' );
 
+if ( current_user_can('contributor') && !current_user_can('upload_files') )
+	add_action('admin_init', 'allow_contributor_uploads');
+
+function allow_contributor_uploads() {
+	$contributor = get_role('contributor');
+	$contributor->add_cap('upload_files');
+}
+
+// remove deafault quicktags
+function wpa_47010( $qtInit ) {
+    $qtInit['buttons'] = 'strong,em,link,ul,ol,li';
+    return $qtInit;
+}
+add_filter('quicktags_settings', 'wpa_47010');
 // add more buttons to the html editor
 function appthemes_add_quicktags() {
     if (wp_script_is('quicktags')){
 ?>
     <script type="text/javascript">
-	QTags.addButton( 'eg_br', 'br', '<br>', '', 'none', 'Horizontal break line', 1 );
-	QTags.addButton( 'eg_header_2', 'h2', '<h2>', '</h2>', 'none', 'Header 2 tag', 2 );
-	QTags.addButton( 'eg_header_3', 'h3', '<h3>', '</h3>', 'none', 'Header 3 tag', 3 );
+	QTags.addButton( 'eg_header_2', 'h2', '<h2>', '</h2>', 'none', 'Header 2 tag', 1 );
+	QTags.addButton( 'eg_header_3', 'h3', '<h3>', '</h3>', 'none', 'Header 3 tag', 2 );
+	QTags.addButton( 'eg_paragraph', 'p', '<p>', '</p>', 'none', 'Paragraph tag', 3 );
     </script>
 <?php
     }
